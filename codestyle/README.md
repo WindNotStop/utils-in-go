a5abbc207c3570716329efedc86e0406c17fa4c4
WindNotStop <1020560484@qq.com>
2020-02-16 00:57:08 +0800

    strategy

--- codestyle/README.md
+++ codestyle/README.md
@@ -3,10 +3,10 @@
 常用代码设计规范
 
 - **withOption** 
- >usage:func(args...,With***(opt),With***(opt))
+ >usage : func(args..., With***(opt), With***(opt))
 
 - **middleware** 
- >usage:service = ***Middleware(middleware)(service)
+ >usage : service = ***Middleware(middleware)(service)
  
  - **strategy** 
-  >usage:Strategy(s*).Start(args...)
+ >usage : Strategy(s*).Start(args...)
--- codestyle/strategy.go
+++ codestyle/strategy.go
@@ -5,28 +5,38 @@ import (
 	"time"
 )
 
+type Attacker interface {
+	attacking()
+}
+
 type List []string
 
-type Strategy func(t Target)
-
-func (s Strategy) Start(l List){
-	t := Target{
-		l:        l,
-		n:        time.Now(),
-	}
-	s(t)
-}
+type Strategy func(t *Target)
 
 type Target struct {
 	l []string
 	n time.Time
+	Strategy
+}
+
+func (t *Target) attacking(){
+	t.Strategy(t)
+}
+
+func (s Strategy) Start(l List){
+	t := &Target{
+		l:        l,
+		n:        time.Now(),
+		Strategy: s,
+	}
+	t.attacking()
 }
 
 func StrategyUsage() {
-	s0 := func(t Target) {fmt.Printf("attack %v at %v\n", t.l[0],t.n)}
-	s1 := func(t Target) {fmt.Printf("attack %v at %v\n", t.l[2],t.n.Add(time.Hour))}
-	s2 := func(t Target) {fmt.Printf("attack %v at %v\n", t.l[1],t.n.AddDate(0,0,1))}
-	l := List{"gg","mm","bb"}
+	s0 := func(t *Target) {fmt.Printf("%v will attack at %v\n", t.l[0],t.n)}
+	s1 := func(t *Target) {fmt.Printf("%v will attack at %v\n", t.l[2],t.n.Add(time.Hour))}
+	s2 := func(t *Target) {fmt.Printf("%v will attack at %v\n", t.l[1],t.n.AddDate(0,0,1))}
+	l := List{"iron-man","spider-man","black-widow"}
 	Strategy(s0).Start(l)
 	Strategy(s1).Start(l)
 	Strategy(s2).Start(l)
